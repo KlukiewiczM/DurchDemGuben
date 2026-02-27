2f0b2da6f2d75719245c6b493ac178b7bfe80889
Klukiewicz.M <Klukiewicz.M@guben.de>
2026-02-27 08:41:24 +0100

    HintTextScript

---
+++ Assets/Scripts/UI/BlinkingText..cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class BlinkingText : MonoBehaviour
+{
+    [SerializeField] private TMP_Text text;
+    [SerializeField] private float speed = 2f; // szybkość migania
+
+    void Update()
+    {
+        if (text == null) return;
+
+        float alpha = Mathf.Abs(Mathf.Sin(Time.unscaledTime * speed));
+        Color c = text.color;
+        c.a = alpha;
+        text.color = c;
+    }
+}
