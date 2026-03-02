bf0e565c7b17f79e0cf001378a269263155ac4a6
Klukiewicz.M <Klukiewicz.M@guben.de>
2026-03-02 15:10:13 +0100

    Player movememnt and camera follow

---
+++ Assets/Scripts/PlayerMovement2D.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class PlayerMovement2D : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Move")]
+    [SerializeField] private float moveSpeed = 6f;
+    [SerializeField] private float jumpForce = 12f;
+
+    [Header("Ground Check")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckRadius = 0.15f;
+    [SerializeField] private LayerMask groundLayer;
+
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        rb.linearVelocity = new Vector2(x * moveSpeed, rb.linearVelocity.y);
+
+        bool isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow)) && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (groundCheck == null) return;
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+    }
+}
