7c333928e8ecf995a56f3fe85c62b3e71181a14d
Klukiewicz.M <Klukiewicz.M@guben.de>
2026-03-04 11:40:13 +0100

    Hearts only as UI

---
+++ Assets/Scripts/UI/HeartsUI.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HeartsUI : MonoBehaviour
+ {
+        [SerializeField] private Image[] hearts; // 3 obrazki
+
+        // na razie tylko test:
+        private void Start()
+        {
+            // upewniamy się że są widoczne
+            SetHearts(3);
+        }
+
+        public void SetHearts(int count)
+        {
+            for (int i = 0; i < hearts.Length; i++)
+            {
+                if (hearts[i] != null)
+                    hearts[i].enabled = i < count;
+            }
+        }
+}
+
