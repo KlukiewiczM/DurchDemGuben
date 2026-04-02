39ab27967b3614b15ecfe758594e92fc888c7329
Klukiewicz.M <Klukiewicz.M@guben.de>
2026-04-02 15:15:36 +0200

    Text and Building

--- Assets/Scripts/BuildAtFinish.cs
+++ Assets/Scripts/BuildAtFinish.cs
@@ -1,12 +1,29 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BuildAtFinish : MonoBehaviour
 {
     [SerializeField] private string playerTag = "PlayerTag";
     [SerializeField] private int requiredCoins = 3;
+
+    [Header("Objects to show")]
     [SerializeField] private GameObject buildingPlaceholder;
+    [SerializeField] private GameObject builderNpc;
+    [SerializeField] private GameObject buildMessage;
+
+    [Header("Next Scene")]
+    [SerializeField] private string nextSceneName = "2LevelScene";
 
     private bool alreadyBuilt = false;
+    private bool waitingForContinue = false;
+
+    private void Update()
+    {
+        if (waitingForContinue && Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(nextSceneName);
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -21,8 +38,16 @@ public class BuildAtFinish : MonoBehaviour
             if (buildingPlaceholder != null)
                 buildingPlaceholder.SetActive(true);
 
+            if (builderNpc != null)
+                builderNpc.SetActive(true);
+
+            if (buildMessage != null)
+                buildMessage.SetActive(true);
+
             alreadyBuilt = true;
-            Debug.Log("Budynek postawiony!");
+            waitingForContinue = true;
+
+            Debug.Log("Budynek postawiony. Czekam na spację.");
         }
         else
         {
